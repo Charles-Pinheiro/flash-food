dc1b54e970c74aed420aadaa582434df8ac64f66
Humberto <matheushumberto520@gmail.com>
2022-01-26 11:35:20 -0400

    fix migration

--- src/database/migrations/1643116060955-RelationProductWithCategory.ts
+++ src/database/migrations/1643116060955-RelationProductWithCategory.ts
@@ -4,14 +4,17 @@ export class RelationProductWithCategory1643116060955 implements MigrationInterf
     name = 'RelationProductWithCategory1643116060955'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-    
+        // await queryRunner.query(`CREATE TABLE "store_category" ("store_category_id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, CONSTRAINT "PK_97c4207e55b7e8d4cc8665052f2" PRIMARY KEY ("store_category_id"))`);
+        // await queryRunner.query(`CREATE TABLE "stores" ("store_id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "categoryId" character varying NOT NULL, "addressId" uuid NOT NULL, "userId" uuid NOT NULL, "storeCategoriesStoreCategoryId" uuid, CONSTRAINT "REL_d5740b38aa467721802eb30f24" UNIQUE ("addressId"), CONSTRAINT "PK_123fd77ff9cffa555180ca737ff" PRIMARY KEY ("store_id"))`);
         await queryRunner.query(`ALTER TABLE "products" ADD "categoryId" uuid`);
         await queryRunner.query(`ALTER TABLE "address" DROP COLUMN "number"`);
         await queryRunner.query(`ALTER TABLE "address" ADD "number" integer NOT NULL`);
         await queryRunner.query(`ALTER TABLE "products" DROP COLUMN "price"`);
         await queryRunner.query(`ALTER TABLE "products" ADD "price" integer NOT NULL`);
         await queryRunner.query(`ALTER TABLE "products" ADD CONSTRAINT "FK_ff56834e735fa78a15d0cf21926" FOREIGN KEY ("categoryId") REFERENCES "products_category"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        
+        //await queryRunner.query(`ALTER TABLE "stores" ADD CONSTRAINT "FK_2b7a7a6d458c6ef149370c5f210" FOREIGN KEY ("storeCategoriesStoreCategoryId") REFERENCES "store_category"("store_category_id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        //await queryRunner.query(`ALTER TABLE "stores" ADD CONSTRAINT "FK_d5740b38aa467721802eb30f24f" FOREIGN KEY ("addressId") REFERENCES "address"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        //await queryRunner.query(`ALTER TABLE "stores" ADD CONSTRAINT "FK_f36d697e265ed99b80cae6984c9" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
