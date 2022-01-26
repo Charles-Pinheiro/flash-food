3eadb4e8a8e93b3f41eae0008f45d86aebe9f91b
Humberto <matheushumberto520@gmail.com>
2022-01-25 22:53:38 -0400

    Feat: Search products by category

--- src/services/Store/ListProductsByStoreService.ts
+++ src/services/Store/ListProductsByStoreService.ts
@@ -4,14 +4,35 @@ import { StoreRepository } from "../../repositories/Store/StoresRepository";
 import AppError from "../../errors/appError";
 import { ProductRepository } from "../../repositories/Product/ProductRepository";
 import Product from "../../models/Products";
+import { CategoryProductRepository } from "../../repositories/Product/CategoryProduct";
 
 
 export default class ListProductByStoreService {
     async execute(request: Request): Promise<Product[]> {
         const productsRepository = getCustomRepository(ProductRepository);
         const storeRepository = getCustomRepository(StoreRepository)
+        const categoryProduct = getCustomRepository(CategoryProductRepository)
         const storeId = request.params.store_id;
-        
+        const sendedCategory = request.query.category;
+
+        if(typeof sendedCategory == "string") {
+            const category = await categoryProduct.findOne({
+                name: sendedCategory
+            });
+
+            if(category) {
+                const products = await productsRepository.find({
+                    where: {
+                        category: category.id
+                    }
+                });
+                return products;
+
+            } else {
+                throw new AppError("No products for this category", 404);
+            };
+        };
+
         try {
             const store = await storeRepository.findOne(storeId);
 
@@ -30,7 +51,7 @@ export default class ListProductByStoreService {
             });
             return products;
 
-        }catch(err) {                        
+        }catch(err) {                                    
             throw new AppError("AppError");
         };
     };
