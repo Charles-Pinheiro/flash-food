f6944453eb486ba53783544353182eb90f297493
Charles Pinheiro <charles_fagundes@hotmail.com>
2022-01-26 17:52:02 -0300

    feat: delete product

--- src/routes/Products/ProductsRoute.ts
+++ src/routes/Products/ProductsRoute.ts
@@ -1,15 +1,24 @@
 import { Router } from "express";
 import CreateProductController from "../../controller/Products/CreateProductController";
+import DeleteProductController from "../../controller/Products/DeleteProductController";
 import Authentication from "../../middlewares/Authentication";
+import storeOwner from "../../middlewares/StoreOwner";
 
 const productRouter = Router();
 
 const createProduct = new CreateProductController();
+const deleteProduct = new DeleteProductController();
 
 productRouter.post(
     "/:store_id",
     Authentication,
     createProduct.handle
+);
+
+productRouter.delete(
+    "/:store_id/product/:id",
+    Authentication,
+    deleteProduct.handle
 );
 
 export default productRouter;
