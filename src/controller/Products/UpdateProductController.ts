87ac9286893ae99b17d20fd6b6485a8090a1ed33
Charles Pinheiro <charles_fagundes@hotmail.com>
2022-01-27 10:32:31 -0300

    fix: pegando erro na delete product

--- src/controller/Products/UpdateProductController.ts
+++ src/controller/Products/UpdateProductController.ts
@@ -7,11 +7,11 @@ export default class UpdateProductController {
         const updateProductService = new UpdateProductService();
 
         const { id } = request.params;
-        return await updateProductService.execute(id, request.body).then(
+        return updateProductService.execute(id, request.body).then(
             res => {return response.json(res)}
         ).catch(
             (err: AppError) => {
                 return response.status(err.statusCode).json({message: err.message})    
         });
-    };
+    }
 }
