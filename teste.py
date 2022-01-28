25b70a4ac57e48348e37d27c91df9e76defd70b6
Diogo <diogosantosferreira.01@gmail.com>
2022-01-27 23:53:05 -0300

    feat: docs

--- teste.py
+++
@@ -1,9 +0,0 @@
-components:
-  securitySchemes:
-    bearerAuth:            # arbitrary name for the security scheme
-      type: http
-      scheme: bearer
-      bearerFormat: JWT    # optional, arbitrary value for documentation purposes
-# 2) Apply the security globally to all operations
-security:
-  - bearerAuth: []
